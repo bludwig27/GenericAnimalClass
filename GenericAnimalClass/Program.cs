04cc60c5c4f98f43f3b901b6278095c561728efb
Benjamin Ludwig <ludwig22@mail.nmc.edu>
2023-03-06 13:17:48 -0500

    Add project files.

---
+++ GenericAnimalClass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GenericAnimalClass
+{
+    class Program
+    {
+        public class Animal<T>
+        {
+            public T data;
+
+            public Animal(T data)
+            {
+                this.data = data;
+            }
+
+            public T getAnimal()
+            {
+                return data;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Animal<string> animalName = new Animal<string>("pangolin");
+            Animal<string> animalHabitat = new Animal<string>("forests and savannahs");
+            Animal<bool> endangered = new Animal<bool>(true);
+            Animal<bool> extinct = new Animal<bool>(false);
+            Animal<int> averageLifespan= new Animal<int>(5);
+
+            Console.WriteLine("Species info:");
+            Console.WriteLine($"Name: {animalName.getAnimal()}");
+            Console.WriteLine($"Habitat: {animalHabitat.getAnimal()}");
+            Console.WriteLine($"Endangered: {endangered.getAnimal()}");
+            Console.WriteLine($"Extinct: {extinct.getAnimal()}");
+            Console.WriteLine($"Average lifespan: {averageLifespan.getAnimal()}");
+        }
+    }
+}
